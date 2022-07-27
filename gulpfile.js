63eec0d76608d6850339cc10affd9e6998682029
Ira <startel2580startel@gmail.com>
2022-07-27 17:43:42 +0300

    Setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,37 @@
-let project_folder = "dest";
-let source_folder = "src";
-
-const gulp        = require('gulp');
 const fileinclude = require('gulp-file-include');
+const gulp = require('gulp');
+const del = require('del');
 
-const paths = {
-  scripts: {
-    src: './',
-    dest: './build/'
-  }
-};
-async function includeHTML(){
-  return gulp.src([
-    '*.html',
-    '!header.html', // ignore
-    '!footer.html' // ignore
-    ])
+gulp.task('html', () => {
+  return gulp.src(['./src/pages/**/*.html'])
     .pipe(fileinclude({
       prefix: '@@',
-      basepath: '@file'
+      basepath: './src'
     }))
-    .pipe(gulp.dest(paths.scripts.dest));
-}
+    .pipe(gulp.dest('./dist'))
+});
 
-function createDist() {
-  return src(['./'], { allowEmpty: true }).pipe(dest('dist/client/scripts'));
-}
-exports.default = includeHTML;
+gulp.task('css', () => {
+  return gulp.src(['./src/css/**/*.*'])
+    .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('js', () => {
+  return gulp.src(['./src/js/**/*.*'])
+    .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('clean', () => {
+  return del('./dist');
+})
+
+gulp.task('watch', () => {
+  gulp.watch('./src/pages/**/*.html', gulp.series('html'));
+  gulp.watch('./src/partials/**/*.html', gulp.series('html'));
+
+  gulp.watch('./src/css/**/*.css', gulp.series('css'))
+
+  gulp.watch('./src/js/**/*.js', gulp.series('js'))
+})
+
+gulp.task('start', gulp.series(['clean', 'html', 'css', 'js', 'watch']))
