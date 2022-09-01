479537c58847a5b6f4276f80c30e57f2e37a42eb
Ira <startel2580startel@gmail.com>
2022-09-01 14:37:07 +0300

    (js)add mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,25 @@
 const burgerEl = document.querySelector('.header-menu__burger');
 const menuEl = document.querySelector('.header-mobile-menu');
 
+///burgerEl.addEventListener('click', () => {
+  menuEl.classList.toggle('active');
+  burgerEl.classList.toggle('active');
+//})
+
+//document.querySelectorAll('.header-mobile-menu').addEventListener('click', () => {
+  //menuEl.classList.remove('active');
+  //burgerEl.classList.remove('active');
+//})
+
+
 burgerEl.onclick = () => menuEl.classList.toggle('active');
+
 
 document.addEventListener('click', (event) => {
   const targetEl = event.target; 
 
-  if (targetEl.closest('.header-mobile-menu') || targetEl.classList.container('.header-mobile-menu')) return; 
-  if (targetEl.closest('.header-menu__burger') || targetEl.classList.container('.header-menu__burger')) return;
+  if (targetEl.closest('.header-mobile-menu') || targetEl.classList.contains('header-mobile-menu')) return; 
+  if (targetEl.closest('.header-menu__burger') || targetEl.classList.contains('header-menu__burger')) return;
 
   menuEl.classList.remove('active');
 })
