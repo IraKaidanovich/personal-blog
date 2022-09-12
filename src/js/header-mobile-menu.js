4d9e0f3a3fe5c1315943183d63ceb694d3edc75f
Ira <startel2580startel@gmail.com>
2022-09-12 16:47:26 +0300

    (mobile menu)fix closing on clik on contatc modal

--- src/js/header-mobile-menu.js
+++ src/js/header-mobile-menu.js
@@ -1,6 +1,5 @@
 const burgerEl = document.querySelector('.header-menu__burger');
 const menuEl = document.querySelector('.header-mobile-menu'); 
-//const contactMobalBody = document.querySelector('.contact-mobal__body');
 
 burgerEl.onclick = () => menuEl.classList.toggle ('is-active');
 
@@ -9,8 +8,7 @@ document.addEventListener('click', (event) => {
 
   if (targetEl.closest('.header-mobile-menu') || targetEl.classList.contains('header-mobile-menu')) return; 
   if (targetEl.closest('.header-menu__burger') || targetEl.classList.contains('header-menu__burger')) return;
-  //if (targetEl.closest('.contact-mobal__body') || targetEl.classList.contains('contact-mobal__body')) return; 
+  if (targetEl.closest('.contact-mobal') || targetEl.classList.contains('contact-mobal')) return; 
 
   menuEl.classList.remove('is-active');
-
 })
